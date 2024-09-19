b7337b1a017e45d0018f1f24a6d7950e9b71a184
Elena Kappler <Elena.Kappler.1003@student.uu.se>
2024-09-19 17:57:12 +0200

    Add project files.

---
+++ EnvironmentCrime/Controllers/InvestigatorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentCrime.Controllers
+{
+    public class InvestigatorController : Controller
+    {
+        public ViewResult CrimeInvestigator()
+        {
+            return View();
+        }
+
+        public ViewResult StartInvestigator()
+        {
+            return View();
+        }
+    }
+}
